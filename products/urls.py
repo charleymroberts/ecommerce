d1d270bce8ae8c063dd5725f89c3c38f6c7e88e3
Charley Roberts <charley.m.roberts@gmail.com>
2023-09-12 20:29:37 +0100

    Add individual product page
    Add urls, views, template and styling for page that displays individual product details (product.html)

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.product_lists, name='product-lists'),
     path('<category_slug>/', views.by_category, name='by-category'),
+    path('<category_slug>/<product_slug>/', views.single_product, name='single-product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product, Category
+from .models import Product, Category, Brand
 
+'''
+View to render...something. Practice one. Delete? 
+Currently what's under products/. Get rid of that page?
+'''
 def product_lists(request):
 
     products = Product.objects.all()
@@ -15,6 +19,10 @@ def product_lists(request):
 
     return render(request, 'products/product_lists.html', context)
 
+
+'''
+View to render the lists of products under each category
+'''
 def by_category(request, category_slug):
 
     category = get_object_or_404(Category, slug=category_slug)
@@ -35,3 +43,31 @@ def by_category(request, category_slug):
     }
 
     return render(request, 'products/category.html', context)
+
+
+'''
+View to render the page for an individual product
+'''
+def single_product(request, category_slug, product_slug):
+
+    products = Product.objects.all()
+    this_product = get_object_or_404(Product, slug=product_slug)
+
+    category = get_object_or_404(Category, slug=category_slug)
+
+    this_brand = this_product.brand
+    related_products = Product.objects.filter(
+        categories__in=this_product.categories.all()).order_by('?')[:4]
+
+    context = {
+        'products': products,
+        'this_product': this_product,
+        'category': category,
+        'this_brand': this_brand,
+        'related_products': related_products,
+    }
+
+    return render(request, 'products/product.html', context)
+
+
+
