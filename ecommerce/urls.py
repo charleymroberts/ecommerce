1027a02920e9bca3ed035f7feee3629f43089a05
Charley Roberts <charley.m.roberts@gmail.com>
2023-09-18 16:52:39 +0100

    Add images and styling to index page and product page
    Add images and styling to index page and individual product page, including the product cards

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
