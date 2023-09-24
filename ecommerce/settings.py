45c0fbffcb673f664573a1763bf4ebd7b9cf0d5a
Charley Roberts <charley.m.roberts@gmail.com>
2023-09-24 18:41:52 +0100

    Deploy to Heroku
    Deployment commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DJANGO_DEBUG') == 'yes')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vegandirect.herokuapp.com', 'localhost']
 
 
 # Application definition
